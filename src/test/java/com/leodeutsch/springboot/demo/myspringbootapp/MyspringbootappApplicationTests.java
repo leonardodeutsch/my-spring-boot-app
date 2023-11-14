18bf7a5cf81ab157addba9e2e351c8019d76b47a
Leo <deutschleonardo@gmail.com>
2023-11-14 14:07:59 -0600

    init

---
+++ src/test/java/com/leodeutsch/springboot/demo/myspringbootapp/MyspringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leodeutsch.springboot.demo.myspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

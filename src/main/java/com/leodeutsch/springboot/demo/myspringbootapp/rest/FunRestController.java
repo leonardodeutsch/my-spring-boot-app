7a88d91a88bd11f9b1a63ea37131fcf80fecba67
Leo <deutschleonardo@gmail.com>
2023-11-14 16:08:53 -0600

    Added custom properties in application.properties and injected those values into rest controller.

--- src/main/java/com/leodeutsch/springboot/demo/myspringbootapp/rest/FunRestController.java
+++ src/main/java/com/leodeutsch/springboot/demo/myspringbootapp/rest/FunRestController.java
@@ -1,10 +1,22 @@
 package com.leodeutsch.springboot.demo.myspringbootapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     // expose "/" that returns "Hello World"
     @GetMapping("/")

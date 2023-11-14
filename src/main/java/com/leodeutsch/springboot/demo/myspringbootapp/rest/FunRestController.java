5ea79698707fa2a49018182b2509c03c4d3d72ef
Leo <deutschleonardo@gmail.com>
2023-11-14 15:31:18 -0600

    Added a few endpoints, enabled spring devtools and actuator, added some properties to the actuator/info endpoint.

---
+++ src/main/java/com/leodeutsch/springboot/demo/myspringbootapp/rest/FunRestController.java
@@ -0,0 +1,28 @@
+package com.leodeutsch.springboot.demo.myspringbootapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // expose "/workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // expose "/fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
+
+}
